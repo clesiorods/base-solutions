8c3b94baf66e6d78f8922d7bbee94620879f59df
clesiorodrigues <clesiano.rodrigues@corpstek.com.br>
2022-09-28 14:15:29 -0300

    Teste de implementação do react-dnd

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import { Finance } from "./pages/Finance";
 import { Home } from "./pages/Home";
 import Kanban from "./pages/Kanban";
 import { Login } from "./pages/Login";
+import Teste from "./pages/Teste";
 
 export function Router() {
   return (
@@ -14,6 +15,7 @@ export function Router() {
       <Route path="/financeiro" element={<Finance />} />
       <Route path="/kanban" element={<Kanban/>} />
       <Route path="/login" element={<Login />} />
+      <Route path="/teste" element={<Teste />} />
     </Routes>
   );
 }
